edc50635a45293adb9e05b8eebeee6a5858b6287
Hyeongjin Kim <kimjin5537@daum.net>
2021-02-07 21:44:12 +0900

    프로필 설정 페이지 추가

--- front/components/main.js
+++ front/components/main.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Head from 'next/head';
+import styled, { createGlobalStyle } from 'styled-components';
+import { Calendar } from 'antd';
+
+const Global = createGlobalStyle`
+  html,
+  body,
+  body > div:first-child,
+  div#__next,
+  div#__next > div {
+    height: 100%;
+  }
+`;
+
+const BackgroundImg = styled.div`
+  border: 0;
+  padding: 0; 
+  background-image: url('https://t1.daumcdn.net/cfile/tistory/26AEB633597644FE1E');
+  min-height: 100%;
+  background-position: center;
+  background-size: cover;
+`;
 
 const Main = () => {
+  const onPanelChange = useCallback((value, mode) => {
+    console.log(value.format('YYYY-MM-DD'), mode);
+  }, []);
+
   return (
     <>
       <Head>
         <meta charSet="utf-8" />
-        <title>로그인 | Feelings</title>
+        <title>Feelings</title>
       </Head>
-      <div>Main</div>
+      <BackgroundImg>
+        <Global />
+        <Calendar onPanelChange={onPanelChange} />
+      </BackgroundImg>
     </>
   );
 };
--- front/pages/firstSetting.js
+++ front/pages/firstSetting.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Head from 'next/head';
+import { Row, Col, Form, Input, Button, Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons'
 
 import StartLayout from '../components/StartLayout';
+import useInput from '../hooks/useInput';
 
 const FirstSetting = () => {
+  const [nickname, onChangeNickname] = useInput('');
+  const [nicknameError, setNicknameError] = useState(false);
+
+  const onSubmit = useCallback(() => {
+    if (nickname === null) {
+      return setNicknameError(true);
+    }
+    console.log(nickname);
+  }, [nickname]);
+
   return (
     <>
       <Head>
@@ -11,7 +24,25 @@ const FirstSetting = () => {
         <title>회원가입 | Feelings</title>
       </Head>
       <StartLayout>
-        <div>회원가입 첫 세팅 페이지</div>
+      <Form onFinish={onSubmit}>
+          <Row>
+            <Col xs={12} md={12} offset={6}>
+              <h2>프로필 설정</h2>
+              <Form.Item>
+                <Avatar size={64} icon={<UserOutlined />} />
+              </Form.Item>
+              <Form.Item>
+                <label htmlFor="user-nick">Nickname</label>
+                <br />
+                <Input name="user-nick" value={nickname} onChange={onChangeNickname} required />
+                {nicknameError && <ErrorMessage>닉네임을 입력해주세요.</ErrorMessage>}
+              </Form.Item>
+              <div>
+                <Button type="primary" htmlType="submit" loading={false}>등록</Button>
+              </div>
+            </Col>
+          </Row>
+        </Form>
       </StartLayout>
     </>
   );
