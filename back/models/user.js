fe98de8b85e57c36e97464ddaad55b5faa2ab2f3
Hyeongjin Kim <kimjin5537@daum.net>
2021-02-27 16:02:01 +0900

    sequelize 모델 & 관계 설정

---
+++ back/models/user.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', { // MySQL에는 users 테이블 생성
+    // id가 기본적으로 들어가있음
+    email: {
+      type: DataTypes.STRING(30),
+      allowNull: false, // 필수
+      unique: true, // 고유한 값
+    },
+    nickname: {
+      type: DataTypes.STRING(30),
+      allowNull: true, // 필수 X
+    },
+    password: {
+      type: DataTypes.STRING(100),
+      allowNull: false, // 필수
+    },
+    img: {
+      type: DataTypes.STRING(200),
+      allowNull: true, // 필수 X
+    },
+  }, {
+    charset: 'utf8',
+    collate: 'utf8_general_ci', // 한글 저장
+  });
+  User.associate = (db) => {
+    db.User.hasMany(db.Post);
+  };
+  return User;
+}
