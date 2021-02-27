00f8d7dec82d8a36254239370a5c6bdbef90e4c9
Hyeongjin Kim <kimjin5537@daum.net>
2021-02-27 13:50:54 +0900

    누락된 커밋

--- back/models/index.js
+++ back/models/index.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config); // sequelize 가 node와 mysql을 연결해준다
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
