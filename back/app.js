5601a8425c7d3195306498121d59e151d2ebf9a3
Hyeongjin Kim <kimjin5537@daum.net>
2021-03-10 19:58:25 +0900

    back app.js update

--- back/app.js
+++ back/app.js
@@ -6,6 +6,8 @@ const passport = require('passport');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const path = require('path');
+const hpp = require('hpp');
+const helmet = require('helmet');
 
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
@@ -22,9 +24,15 @@ db.sequelize.sync()
 
 passportConfig();
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+  app.use(hpp());
+  app.use(helmet());
+} else {
+  app.use(morgan('dev'));
+}
 app.use(cors({
-  origin: 'http://localhost:3060', // 주소
+  origin: ['http://localhost:3060', 'feelings.com'], // 주소
   credentials: true, // 쿠키 전달
 }));
 app.use('/', express.static(path.join(__dirname, 'uploads')));
